0cf4168d01b82fb7462ad48dd40f9c8fc1f28388
Filippo Albertini <filippo@filippoalbertini.it>
2024-11-25 09:05:06 +0100

    Aggiungere i file di progetto.

---
+++ AS2425.3F.INF.Prof.IndovinaNumero/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AS2425._3F.INF.Prof.IndovinaNumero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
